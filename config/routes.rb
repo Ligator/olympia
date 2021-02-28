6a0e60aeb3b704cec40e2771e45082e13f8b7758
Abel Pacheco <abelpacheco@outlook.com>
2021-02-28 00:57:36 -0600

    Set root and move the email test (#10)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   resources :stores
   devise_for :users
-  get 'statics/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "statics/home"
+  get "statics/test_email"
+  root to: "statics#home"
 end
