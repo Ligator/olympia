cefd7006698e4fdac3c5560d64f537136a2693bf
Abel Pacheco <abelpacheco@outlook.com>
2021-02-28 01:43:53 -0600

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_02_28_022307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-    create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
