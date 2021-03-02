31edc9b1c6c08b25a30e255e0dc962659e61e3f5
davepcage <71347825+davepcage@users.noreply.github.com>
2021-03-02 11:56:15 -0600

    internacinalizacion devise

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Olympia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :es
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -21,3 +20,4 @@ module Olympia
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
