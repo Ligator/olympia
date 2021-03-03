c4ebb49e9b57877084459023b398fe8c80795b9b
AlfredoJA <alfre.jsang@hotmail.com>
2021-03-03 12:01:00 -0600

    changed method index in search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,7 @@
 class SearchesController < ApplicationController
 
   def index
-    if params[:filter].blank?
-      @users = ''
-      @stores = ''
-      @products = ''
-    else
+    if params[:filter].present?
       @users = User.where(["first_name ILIKE ? OR last_name ILIKE ?", "%#{params[:filter]}%", "%#{params[:filter]}%"])
       @stores = Store.where("name ILIKE ? OR description ILIKE ?", "%#{params[:filter]}%", "%#{params[:filter]}%")
       @products = Product.where("name ILIKE ? OR description ILIKE ?", "%#{params[:filter]}%", "%#{params[:filter]}%")
