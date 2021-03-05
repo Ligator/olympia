9abcfebbb088e6fd72595d51ce64e556e16b029a
Abel Pacheco <abelpacheco@outlook.com>
2021-03-05 15:05:18 -0600

    Fix JS issues with Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,17 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "../stylesheets/application"
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+
+document.addEventListener("turbolinks:load", () => {
+  JQuery("[data-toggle='tooltip']").tooltip()
+  JQuery("[data-toggle='popover']").popover()
+})
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "bootstrap"
-import "../stylesheets/application"
-document.addEventListener("turbolinks:load", function() {
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-  })
-})
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,8 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+    'window.jQuery': 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
   })
 )
