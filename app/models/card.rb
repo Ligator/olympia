1bee5fb58b768344da09e2f02a6d4e4b3e140523
AlfredoJA <alfre.jsang@hotmail.com>
2021-03-05 21:24:57 -0600

    Updated show products,edit,new and update forms

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ApplicationRecord
   belongs_to :user
-  validates_presence_of :number_card, :name_bank, :titular_name, :expiration_date
+  validates_presence_of :number_card, :name_bank, :titular_name
 end
