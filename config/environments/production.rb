97c7d4998da3c31f15e8c5c37460655b232ca457
Abel Pacheco <abelpacheco@outlook.com>
2021-03-06 05:02:17 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.action_mailer.asset_host = 'https://coartesanos.herokuapp.com'
+  config.action_mailer.asset_host = 'https://colordemipueblo.com'
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
@@ -98,6 +98,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.zoho.com',
+    port:                 587,
+    domain:               'colordemipueblo.com',
+    user_name:            'support@colordemipueblo.com',
+    password:             ENV["COLORDEMIPUEBLO_PWD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
